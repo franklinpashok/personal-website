417249b1f32b23934a8fdad5a88c6449278cabb6
Franklin <franklinpashok@gitlab.com>
2025-08-17 22:10:33 +0200

    add github actions workflow

--- infra/site-setup/github_oidc.tf
+++ infra/site-setup/github_oidc.tf
@@ -1,15 +1,15 @@
 module "github_oidc_provider" {
     source = "../../modules/base/iam_github_oidc"
-    
+
     aws_app_name = local.app_metadata.name
 
     github_repos = {
         "mini-site" = {
-        github_repo         = "franklinpashok/personal-website"
-        github_branches     = ["main"]
-        github_environments = ["prd", ]
-        role_name           = null
-        role_path           = var.role_path
-        }
+            github_repo         = "franklinpashok/personal-website"
+            github_branches     = ["main"]
+            github_environments = []
+            role_name           = null
+            role_path           = var.role_path
+            }
     }
 }
