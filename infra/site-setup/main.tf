d3c44c698c30850df35f7b1b471ac86b43bdd028
Franklin <franklinpashok@gitlab.com>
2026-01-04 21:09:31 +0100

    add acm validation

--- infra/site-setup/main.tf
+++ infra/site-setup/main.tf
@@ -1,7 +1,7 @@
 module "my_personal_website" {
   source = "../../modules/base/static_site/"
 
-  bucket_name        = "my-personal-website"
+  bucket_name        = "franklinpulltikurthi-personal-website"
   bucket_domain_name = module.my_personal_website.s3_bucket_bucket_domain_name
   acm_domain_name    = data.aws_route53_zone.personal_website.name
   route53_zone_id    = data.aws_route53_zone.personal_website.zone_id
